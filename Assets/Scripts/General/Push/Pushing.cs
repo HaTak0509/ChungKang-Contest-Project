9c7cf738b34148aa90fcf55b04e7c355e33d8e9d
HaTak <lgwoohatak@gmail.com>
2025-12-30 22:14:08 +0900

    PushObject Modify

--- Assets/Scripts/General/Push/Pushing.cs
+++ Assets/Scripts/General/Push/Pushing.cs
@@ -1,72 +1,69 @@
 using UnityEngine;
 
-public class Pushing : MonoBehaviour
-{
-    public bool leftPush;
-    public bool rightPush;
-
+public class Pushing : MonoBehaviour 
+{ 
+    public bool leftPush; 
+    public bool rightPush; 
     private PushingObject _pushingOb;
     private Rigidbody2D playerRb;
 
-    private void Awake()
-    {
-        playerRb = GetComponent<Rigidbody2D>();
+    private void Awake() 
+    { 
+        playerRb = GetComponent<Rigidbody2D>(); 
     }
-
-    private void FixedUpdate()
-    {
+    
+    private void FixedUpdate() 
+    { 
         if (leftPush && _pushingOb != null)
-        {
+        { 
             if (!leftPush)
-            {
+            { 
                 _pushingOb.Stop();
-                _pushingOb = null;
-            }
+                _pushingOb = null; 
+            } 
             Vector2 moveDir = playerRb.velocity.normalized;
             _pushingOb.Push(moveDir);
         }
-
         if (rightPush && _pushingOb != null)
         {
             if (!rightPush)
-            {
+            { 
                 _pushingOb.Stop();
                 _pushingOb = null;
-            }
-            Vector2 moveDir = playerRb.velocity.normalized;
+            } 
+            Vector2 moveDir = playerRb.velocity.normalized; 
             _pushingOb.Push(moveDir);
-        }
+        } 
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
-    {
+    { 
         if (collision.gameObject.TryGetComponent(out PushingObject pushingObject))
         {
             _pushingOb = pushingObject;
-
             if (transform.position.x > _pushingOb.gameObject.transform.position.x)
             {
                 leftPush = true;
-            }
-            else if (transform.position.x < _pushingOb.gameObject.transform.position.x)
-            {
+            } 
+            else if (transform.position.x < _pushingOb.gameObject.transform.position.x) 
+            { 
                 rightPush = true;
             }
         }
-    }
-
-    private void OnCollisionExit2D(Collision2D collision)
-    {
+    } 
+    
+    private void OnCollisionExit2D(Collision2D collision) 
+    { 
         if (collision.gameObject.TryGetComponent(out PushingObject pushingObject))
         {
-            if (transform.position.x > _pushingOb.gameObject.transform.position.x)
-            {
+            if (transform.position.x > _pushingOb.gameObject.transform.position.x) 
+            { 
                 leftPush = false;
             }
             else if (transform.position.x < _pushingOb.gameObject.transform.position.x)
             {
                 rightPush = false;
-            }
-        }
-    }
+            } 
+        } 
+    } 
 }
--- Assets/Scripts/General/Push/PushingObject.cs
+++ Assets/Scripts/General/Push/PushingObject.cs
@@ -1,24 +1,20 @@
 using UnityEngine;
-
-public class PushingObject : MonoBehaviour
+public class PushingObject : MonoBehaviour 
 {
-    public bool isPushing;
-    public float pushForce = 5f;
+    private Rigidbody2D rb; 
 
-    private Rigidbody2D rb;
-
-    private void Awake()
-    {
+    private void Awake() 
+    { 
         rb = GetComponent<Rigidbody2D>();
-    }
+    } 
 
     public void Push(Vector2 direction)
     {
-        rb.velocity = new Vector2(direction.x * pushForce, rb.velocity.y);
+        rb.velocity = new Vector2(direction.x, rb.velocity.y).normalized;
     }
-
+    
     public void Stop()
     {
         rb.velocity = new Vector2(0, rb.velocity.y);
-    }
+    } 
 }
