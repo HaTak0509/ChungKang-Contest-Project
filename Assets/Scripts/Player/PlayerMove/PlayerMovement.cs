bdc31a82d25821a79999243a64586d1138195e0f
HaTak <lgwoohatak@gmail.com>
2026-01-02 23:38:59 +0900

    Flotation Developing

--- Assets/Scripts/Player/PlayerMove/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMove/PlayerMovement.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] float walkSpeed = 6f;
-    [SerializeField] float pushingSpeed = 4; 
+    [SerializeField] float pushingSpeed = 4;
+
+    public bool moveLimit; 
 
     private Rigidbody2D _rb2D;
     private Damageable _damageable;
@@ -44,6 +46,12 @@ public class PlayerMovement : MonoBehaviour
             return;
         }
 
+        if (moveLimit)
+        {
+            _rb2D.velocity = new Vector2(0, _rb2D.velocity.y);
+            return;
+        }
+
         // 왼쪽 벽에 닿은 상태에서 왼쪽을 보고 있다면
         if (_touchingDetection.WallDirection == -1 && transform.localScale.x < 0)
         {
@@ -53,7 +61,7 @@ public class PlayerMovement : MonoBehaviour
         {
             _rb2D.velocity = new Vector2(_moveInput.x * CurrentSpeed, _rb2D.velocity.y);
         }
-        
+
         // 오른쪽 벽에 닿은 상태에서 오른쪽을 보고 있다면
         if (_touchingDetection.WallDirection == 1 && transform.localScale.x > 0)
         {
--- Assets/Scripts/Puzzle/Flotation.cs
+++ Assets/Scripts/Puzzle/Flotation.cs
@@ -1,19 +1,52 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Flotation : MonoBehaviour
 {
-    [SerializeField] private GameObject player;
-    [SerializeField] private float flotationForce;
+    [SerializeField] private float flotationForce = 5f;
+    [SerializeField] private float maxUpVelocity = 2f;
 
-    public bool action;
+    private bool action;
 
-    private void Update()
+    private PlayerMovement _playerMovement;
+    private Rigidbody2D _playerRb;
+
+    private void FixedUpdate()
     {
-        if (action && !player)
-        {
+        if (!action || _playerRb == null) return;
 
+        // À§·Î ºÎÀ¯ Èû
+        if (_playerRb.velocity.y < maxUpVelocity)
+        {
+            _playerRb.velocity = new Vector2(
+                _playerRb.velocity.x,
+                Mathf.Lerp(
+                    _playerRb.velocity.y,
+                    flotationForce,
+                    Time.fixedDeltaTime * 3f
+                )
+            );
         }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (!collider.CompareTag("Player")) return;
+
+        action = true;
+        _playerRb = collider.GetComponent<Rigidbody2D>();
+        _playerMovement = collider.GetComponent<PlayerMovement>();
+        _playerMovement.moveLimit = true;
+
+        _playerRb.velocity = Vector2.zero;
+    }
+
+    private void OnTriggerExit2D(Collider2D collider)
+    {
+        if (!collider.CompareTag("Player")) return;
+
+        action = false;
+        _playerMovement.moveLimit = false;
+        _playerRb = null;
+        _playerMovement = null;
     }
 }
