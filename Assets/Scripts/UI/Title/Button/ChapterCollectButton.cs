8bcd910049f08c22f1d2601a3c1cad4ffc1a0259
Zoropic <chotaeseong2007@gmail.com>
2026-02-12 04:02:47 +0900

    애니메이션 추가

--- Assets/Scripts/UI/Title/Button/ChapterCollectButton.cs
+++ Assets/Scripts/UI/Title/Button/ChapterCollectButton.cs
@@ -1,3 +1,5 @@
+using Cysharp.Threading.Tasks;
+using Unity.VisualScripting.Antlr3.Runtime;
 using UnityEngine;
 
 public class ChapterCollectButton : MonoBehaviour
@@ -5,11 +7,33 @@ public class ChapterCollectButton : MonoBehaviour
     [SerializeField] GameObject title;
     [SerializeField] GameObject chaptercollect;
     [SerializeField] GameObject exit;
+    [SerializeField] Animator _animator;
+    public async void OnchapterChange()
+    {
 
-    public void OnchapterChange()
+        exit.SetActive(!exit.activeSelf);
+
+        PlayAndExecute("SelectChapter").Forget();
+    }
+
+    public async UniTaskVoid PlayAndExecute(string animName)
+    {
+
+        _animator.Play(animName);
+
+        await UniTask.Yield(PlayerLoopTiming.Update);
+
+        var stateInfo = _animator.GetCurrentAnimatorStateInfo(0);
+
+        await UniTask.Delay(System.TimeSpan.FromSeconds(stateInfo.length));
+
+
+        ExecuteNextLogic();
+    }
+    public void ExecuteNextLogic()
     {
         title.SetActive(!title.activeSelf);
+
         chaptercollect.SetActive(!chaptercollect.activeSelf);
-        exit.SetActive(!exit.activeSelf);
     }
 }
--- Assets/Scripts/UI/Title/Button/TitleExitButton.cs
+++ Assets/Scripts/UI/Title/Button/TitleExitButton.cs
@@ -4,9 +4,12 @@ public class TitleExitButton : MonoBehaviour
 {
     [SerializeField] private GameObject title;
     [SerializeField] private GameObject ChapterCollect;
+    [SerializeField] private GameObject buttons;
+
 
     public void OnExit()
     {
+        buttons.SetActive(!buttons.activeSelf);
         title.SetActive(!title.activeSelf);
         ChapterCollect.SetActive(!ChapterCollect.activeSelf);
     }
