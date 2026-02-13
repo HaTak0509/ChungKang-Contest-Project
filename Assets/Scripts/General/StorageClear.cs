2cdaaec19a198af65c8289851cdc4204718b1dd7
HaTak <lgwoohatak@gmail.com>
2026-02-13 20:54:25 +0900

    ...

---
+++ Assets/Scripts/General/StorageClear.cs
@@ -0,0 +1,14 @@
+// using UnityEditor;
+// using UnityEngine;
+// 
+// public class StorageClear : Editor
+// {
+//     [MenuItem("Tools/Clear All Storage")]
+//     public static void ClearAllStorage()
+//     {
+//         PlayerPrefs.DeleteAll();
+//         PlayerPrefs.Save();
+// 
+//         EditorUtility.DisplayDialog("저장소 초기화", "모든 PlayerPrefs 데이터가 삭제되었습니다.", "확인");
+//     }
+// }
